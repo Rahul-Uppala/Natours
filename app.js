07aa21ce6d2eeae667315819696ecaa06bccedef
Rahul Uppala <rahul.uppala4599@gmail.com>
2026-03-17 11:29:01 +0530

    Added a comment

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ const viewRouter = require('./routes/viewRoutes');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
+//start express App
 const app = express();
 
 app.set('view engine', 'pug');
